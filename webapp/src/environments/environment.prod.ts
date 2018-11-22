5986e167b3e0d8d5816947ae6d6ec7dfbfda2c6e
Nikita Korobkov <nikkorobk@gmail.com>
2018-11-22 14:29:24 +0300

    compose to run both apps

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mock_api: false,
+  base_url: 'http://elasticsearch:9200',
+  search_request_template: '/books-index/book/_search'
 };
