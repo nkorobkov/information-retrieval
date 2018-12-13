7dd0d43fa0e17eec0949242e6ef8f3a3fc5134a4
Daniil Gavrilov <olavursky@gmail.com>
2018-12-13 22:35:22 +0300

    Server added

--- backend/ner/ner_extractor.py
+++ backend/ner/ner_extractor.py
@@ -1,7 +1,7 @@
-import asyncio
 import ujson
 from collections import defaultdict
 
+import numpy as np
 from natasha import NamesExtractor
 
 
@@ -11,18 +11,25 @@ class NERExtractor:
     def names(self, text):
         return [match.fact for match in self.extractor(text)]
 
-    async def names_by_id(self, id):
+    def names_by_id(self, id):
         try:
             with open("../data/{}.json".format(id)) as file:
                 book = ujson.loads(file.read())
                 names = [self.format_name(name) for name in self.names(book["text"])]
+
                 result = defaultdict(int)
                 for name in names:
                     result[name] += 1
 
-                return dict(result)
+                return self.most_freq(dict(result))
         except:
             raise KeyError
+
+    @staticmethod
+    def most_freq(names: dict):
+        names = [(name, names[name]) for name in names.keys()]
+        indices = np.array([k for name, k in names]).argsort()[-3:][::-1]
+        return [names[i][0] for i in indices]
 
     @staticmethod
     def format_name(name):
@@ -36,7 +43,5 @@ class NERExtractor:
 
 if __name__ == "__main__":
     extractor = NERExtractor()
-    loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(extractor.names_by_id(101))
+    result = extractor.names_by_id(101)
     print(result)
-    loop.close()
